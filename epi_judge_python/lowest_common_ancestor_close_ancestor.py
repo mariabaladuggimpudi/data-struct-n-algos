7070e86a4aee3dc4490e6a38d7f1e8cb6680d397
Bala Duggimpudi <mariabaladuggimpudi@gmail.com>
2023-07-03 09:12:43 -0400

    Common Ancestor with least time

--- epi_judge_python/lowest_common_ancestor_close_ancestor.py
+++ epi_judge_python/lowest_common_ancestor_close_ancestor.py
@@ -11,6 +11,20 @@ from test_framework.test_utils import enable_executor_hook
 def lca(node0: BinaryTreeNode,
         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
     # TODO - you fill in here.
+
+    visited_nodes = set()
+    while node0 or node1:
+        if node0 and node0 in visited_nodes:
+            return node0
+        visited_nodes.add(node0)
+        if node0:
+            node0 = node0.parent
+        if node1 and node1 in visited_nodes:
+            return node1
+        visited_nodes.add(node1)
+        if node1:
+            node1 = node1.parent
+
     return None
 
 
