bc62f4e0227bac0a0028166726f9fefdbf1b0e9c
Bala Duggimpudi <mariabaladuggimpudi@gmail.com>
2023-07-05 08:53:09 -0400

    Solved longest subarray using hash tables

--- epi_judge_python/longest_subarray_with_distinct_values.py
+++ epi_judge_python/longest_subarray_with_distinct_values.py
@@ -5,15 +5,17 @@ from test_framework import generic_test
 
 def longest_subarray_with_distinct_entries(A: List[int]) -> int:
     # TODO - you fill in here.
-    word_n_id, start, long_dist = {}, 0, float('-inf')
+    word_n_id, start, long_dist = {}, 0, 0
     for index, num in enumerate(A):
         if str(num) in word_n_id:
-            long_dist = max(long_dist, len(A[start:index]))
-            start = word_n_id[str(num)] + 1
-            print(long_dist, start)
-        word_n_id[str(num)] = index
+            dup_id = word_n_id[str(num)]
+            if dup_id >= start:
+                long_dist = max(long_dist, len(A[start:index]))
+                start = word_n_id[str(num)] + 1
 
-    return long_dist
+        word_n_id[str(num)] = index
+    #long_dist = max(long_dist, len(word_n_id))
+    return max(long_dist, len(A) - start)
 
 
 if __name__ == '__main__':
