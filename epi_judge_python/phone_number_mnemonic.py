ad3aa7ac27f19439e6e1c6511c5faee5473dbc59
Bala Duggimpudi <mariabaladuggimpudi@gmail.com>
2023-07-30 20:38:02 -0400

    Phone Number Mnemonic Solved

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -2,10 +2,23 @@ from typing import List
 
 from test_framework import generic_test, test_utils
 
-
+mapping = ('0', '1', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ')
 def phone_mnemonic(phone_number: str) -> List[str]:
     # TODO - you fill in here.
-    return []
+    def phone_mnemonic_helper(digit):
+        if digit == len(phone_number):
+            result.append(''.join(partial_mnemonic))
+        else:
+            for c in mapping[int(phone_number[digit])]:
+                partial_mnemonic[digit] = c
+                phone_mnemonic_helper(digit+1)
+
+
+
+    result = []
+    partial_mnemonic = ['0'] * len(phone_number)
+    phone_mnemonic_helper(0)
+    return result
 
 
 if __name__ == '__main__':
