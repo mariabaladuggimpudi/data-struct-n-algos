8cccbb0ad8e39d07a7f075617dc401c41db4ecac
Bala Duggimpudi <mariabaladuggimpudi@gmail.com>
2023-08-12 21:06:54 -0400

    DP total score comb

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -6,7 +6,15 @@ from test_framework import generic_test
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
     # TODO - you fill in here.
-    return 0
+
+    num_of_scores = [[1] + [0] * final_score for _ in range(len(individual_play_scores))]
+    for i in range(len(individual_play_scores)):
+        for j in range(1, final_score+1):
+            without_curr_play = num_of_scores[i-1][j] if i >= 1 else 0
+            with_curr_play = num_of_scores[i][j - individual_play_scores[i]] if j >= individual_play_scores[i] else 0
+            num_of_scores[i][j] = with_curr_play + without_curr_play
+
+    return num_of_scores[-1][-1]
 
 
 if __name__ == '__main__':
