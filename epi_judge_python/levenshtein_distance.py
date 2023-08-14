cdf207cd308b2c17313f5090b5466c9e8c371358
Bala Duggimpudi <mariabaladuggimpudi@gmail.com>
2023-08-14 10:37:45 -0400

    Added DP levenshtein Prob

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -3,7 +3,26 @@ from test_framework import generic_test
 
 def levenshtein_distance(A: str, B: str) -> int:
     # TODO - you fill in here.
-    return 0
+    # Converting the string 'A' to string 'B'
+    def levenshtein_distance_helper(A_idx, B_idx):
+
+        if A_idx < 0:
+            return B_idx + 1
+        if B_idx < 0:
+            return A_idx + 1
+        if store_distance_2d[A_idx][B_idx] == 0:
+            if A[A_idx] == B[B_idx]:
+                store_distance_2d[A_idx][B_idx] = levenshtein_distance_helper(A_idx - 1, B_idx -1)
+            else:
+                substitute = levenshtein_distance_helper(A_idx-1, B_idx-1)
+                add = levenshtein_distance_helper(A_idx, B_idx-1)
+                delete = levenshtein_distance_helper(A_idx - 1, B_idx)
+
+                store_distance_2d[A_idx][B_idx] = 1 + min(substitute, add, delete)
+
+        return store_distance_2d[A_idx][B_idx]
+    store_distance_2d = [[0] * len(B) for _ in range(len(A))]
+    return levenshtein_distance_helper(len(A) - 1, len(B) - 1)
 
 
 if __name__ == '__main__':
