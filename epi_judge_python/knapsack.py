5af56fcf99d1f8597c861d1bd10a55dfdb6df4a0
Bala Duggimpudi <mariabaladuggimpudi@gmail.com>
2023-08-25 21:52:48 -0400

    Moved the spaces

--- epi_judge_python/knapsack.py
+++ epi_judge_python/knapsack.py
@@ -22,8 +22,6 @@ def optimum_subject_to_capacity(items: List[Item], capacity: int) -> int:
         return capacity_2D[k][available_capacity]
 
 
-
-
     capacity_2D = [[-1] * (capacity + 1) for _ in range(len(items))]
     return optimum_subject_to_capacity_helper(len(items) -1, capacity)
 
