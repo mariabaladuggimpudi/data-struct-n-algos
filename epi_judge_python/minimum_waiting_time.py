d7b2e7cb4f17e0b7ae4d77da6416783aa667fc14
Bala Duggimpudi <mariabaladuggimpudi@gmail.com>
2023-08-26 13:17:20 -0400

    Added Wait Time

--- epi_judge_python/minimum_waiting_time.py
+++ epi_judge_python/minimum_waiting_time.py
@@ -5,7 +5,14 @@ from test_framework import generic_test
 
 def minimum_total_waiting_time(service_times: List[int]) -> int:
     # TODO - you fill in here.
-    return 0
+
+    service_times.sort()
+    tot_wait_time = 0
+    for i, query_time in enumerate(service_times):
+        num_of_queries_remaining = len(service_times) - (i+1)
+        tot_wait_time += num_of_queries_remaining * query_time
+
+    return tot_wait_time
 
 
 if __name__ == '__main__':
