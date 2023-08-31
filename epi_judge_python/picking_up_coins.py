18b00210f2f061a301fc01bbcbd7738065459a0c
Bala Duggimpudi <mariabaladuggimpudi@gmail.com>
2023-08-31 09:15:38 -0400

    Added picking up max gain coins

--- epi_judge_python/picking_up_coins.py
+++ epi_judge_python/picking_up_coins.py
@@ -5,7 +5,24 @@ from test_framework import generic_test
 
 def maximum_revenue(coins: List[int]) -> int:
     # TODO - you fill in here.
-    return 0
+
+    def max_rev_helper(a, b):
+        if a > b:
+            return 0
+        if coins_2d_processed[a][b] == -1:
+            max_rev_a = coins[a] + min(
+                max_rev_helper(a+2, b),
+                max_rev_helper(a+1, b-1))
+            max_rev_b = coins[b] + min(
+                max_rev_helper(a, b-2),
+                max_rev_helper(a+1, b-1)
+            )
+            coins_2d_processed[a][b] = max(max_rev_a, max_rev_b)
+
+        return coins_2d_processed[a][b]
+
+    coins_2d_processed = [[-1] * len(coins) for _ in range(len(coins))]
+    return max_rev_helper(0, len(coins) - 1)
 
 
 if __name__ == '__main__':
